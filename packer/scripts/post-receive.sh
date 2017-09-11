d903a1b91431d27e72fad7bdc3fa7fa9e5aa46a9
arkeros <rafael@arque.ro>
2017-09-11 22:23:15 +0200

    FIX ENOMEM: out of memory when installing packages

--- packer/scripts/post-receive.sh
+++ packer/scripts/post-receive.sh
@@ -2,6 +2,13 @@
 
 chown -R pixel /home/pixel
 chmod -R 0755 /home/pixel/www/public
+
+# FIX ENOMEM: out of memory when installing packages
+# https://stackoverflow.com/questions/26193654/node-js-catch-enomem-error-thrown-after-spawn
+fallocate -l 4G /swapfile
+mkswap /swapfile
+swapon /swapfile
+
 su - pixel
 cd /home/pixel/www
 yarn install
